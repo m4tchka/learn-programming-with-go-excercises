05e8b0161dece21eee7a4340ee387b45ad94296d
m4tchka <91279022+m4tchka@users.noreply.github.com>
2022-10-18 22:37:08 +0100

    4-11 recursive solution

--- 4-control-flow-structures/11-factorial/main.go
+++ 4-control-flow-structures/11-factorial/main.go
@@ -13,7 +13,7 @@ Write a program which takes as input a number N and calculates N! (factorial).
 The factorial of a number is the multiplication of the numbers [1; N]
 */
 
-func main() {
+/* func main() {
 	r := bufio.NewReader(os.Stdin)
 	line, err := r.ReadString('\n')
 	if err != nil {
@@ -28,4 +28,29 @@ func main() {
 		result *= i
 	}
 	fmt.Println(result)
+} */
+
+func main() {
+	n := getInput()
+	f := calculateFactorial(n, 1)
+	fmt.Printf("\nFactorial: %d", f)
+}
+func getInput() int {
+	r := bufio.NewReader(os.Stdin)
+	line, err := r.ReadString('\n')
+	if err != nil {
+		panic(err)
+	}
+	num, err := strconv.Atoi(strings.TrimSpace(line))
+	if err != nil {
+		panic(err)
+	}
+	return num
+}
+func calculateFactorial(num, total int) int {
+	if num == 1 {
+		return total
+	} else {
+		return calculateFactorial(num-1, total*num)
+	}
 }
