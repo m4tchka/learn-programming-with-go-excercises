ecc9db0da265b45311aea803bf9c349a541bf938
m4tchka <91279022+m4tchka@users.noreply.github.com>
2022-10-23 12:22:56 +0100

    5-04

---
+++ 5-arrays-slices-and-maps/04-union-of-numbers/go.mod
@@ -0,0 +1,3 @@
+module 04-union-of-numbers
+
+go 1.19
---
+++ 5-arrays-slices-and-maps/04-union-of-numbers/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+/*
+Write a program which takes in two lines of numbers and outputs the union of the two lists. There should be no duplicates.
+*/
+
+func main() {
+	sli1 := getNumSlice()
+	sli2 := getNumSlice()
+	var sliU []string
+	sliU = append(sliU, sli1...)
+	for _, elem := range sli2 {
+		if !checkContains(sliU, elem) {
+			sliU = append(sliU, elem)
+		} else {
+			continue
+		}
+	}
+	fmt.Println(strings.Join(sliU, " "))
+}
+
+func getNumSlice() []string {
+	r := bufio.NewReader(os.Stdin)
+	line, err := r.ReadString('\n')
+	if err != nil {
+		panic(err)
+	}
+	numStrSli := strings.Split(strings.TrimSpace(line), " ")
+	if err != nil {
+		panic(err)
+	}
+	return numStrSli
+}
+func checkContains(strSli []string, e string) bool {
+	for _, elem := range strSli {
+		if elem == e {
+			return true
+		}
+	}
+	return false
+}
